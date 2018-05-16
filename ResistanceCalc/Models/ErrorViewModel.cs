6543c1929eaa635235b315761338740cefe88515
Thomas Trenta <thomas.trenta@gmail.com>
2018-05-15 21:51:18 -0500

    Add project files.

---
+++ ResistanceCalc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ResistanceCalc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
